d7932022a938be15c01db06d04c40ac968f91160
joeahkim <joeahkim@gmail.com>
2026-02-09 11:05:35 +0300

    Token sign in if we have a token saved on the device

--- app/src/main/java/com/joeahkim/carrental/navigation/AppNavigation.kt
+++ app/src/main/java/com/joeahkim/carrental/navigation/AppNavigation.kt
@@ -2,20 +2,39 @@
 package com.joeahkim.carrental.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.joeahkim.carrental.data.local.DataStoreManager
 import com.joeahkim.carrental.ui.main.MainScreen
 import com.joeahkim.carrental.ui.login.LoginScreen
 import com.joeahkim.carrental.ui.login.SignUpScreen
+import kotlinx.coroutines.flow.firstOrNull
 
 @Composable
 fun AppNavigation() {
+    val context = LocalContext.current
+    val dataStoreManager = remember { DataStoreManager(context) }
+    val startDestination = remember { mutableStateOf<Routes?>(null) }
+
+    LaunchedEffect(Unit) {
+        val token = dataStoreManager.getToken().firstOrNull()
+        startDestination.value = if (!token.isNullOrEmpty()) Routes.Main else Routes.Login
+    }
+
+    val destination = startDestination.value ?: return
+
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = Routes.Login
+        startDestination = destination
     ) {
         composable<Routes.Login> {
             LoginScreen(
